d230a1cc804898d7bf169459a4fcc64dc0802f79
Millan <mathilde.millan@inrae.fr>
2023-05-17 10:27:31 +0200

    add commented text for later usage

--- sim_vsp/main_sim.py
+++ sim_vsp/main_sim.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from itertools import product
-from json import load
+from json import load as load_json
 from multiprocessing import Pool
 from pathlib import Path
 from typing import Iterable
@@ -22,10 +22,10 @@ def run_hydroshoot(path_weather: Path, params: dict, path_preprocessed_dir: Path
         path_mtg=str(path_preprocessed / 'initial_mtg.pckl'),
         path_geometry=str(path_preprocessed / 'geometry.bgeom'))
 
-    with open(path_preprocessed / 'static.json') as f:
-        static_inputs = load(f)
-    with open(path_preprocessed / f'dynamic_{date_info[0]}.json') as f:
-        dynamic_inputs = load(f)
+    with open(path_preprocessed / 'static.json', mode='r') as f:
+        static_inputs = load_json(f)
+    with open(path_preprocessed / f'dynamic_{date_info[0]}.json', mode='r') as f:
+        dynamic_inputs = load_json(f)
 
     path_output = path_output_dir / date_info[0] / f'combi_{id_sim}'
     path_output.mkdir(parents=True, exist_ok=True)
@@ -59,6 +59,19 @@ if __name__ == '__main__':
     path_root = Path(__file__).parent
     cfg = ConfigSimVSP()
 
+    # sky = 'clear_sky'
+    # id_combi = 27
+    #
+    # date_index = 1 if sky == 'cloudy_sky' else 0
+    #
+    # run_hydroshoot(
+    #     path_weather=cfg.path_weather,
+    #     params=cfg.params,
+    #     path_preprocessed_dir=cfg.path_preprocessed_inputs,
+    #     id_sim=id_combi,
+    #     path_output_dir=cfg.path_outputs,
+    #     date_info=cfg.dates[date_index])
+
     ids = sorted([v['id'] for v in get_architectural_params(cfg.path_params_architecture)])
 
     time_on = datetime.now()
@@ -67,3 +80,26 @@ if __name__ == '__main__':
         nb_cpu=12)
     time_off = datetime.now()
     print(f"--- Total runtime: {(time_off - time_on).seconds} sec ---")
+
+    # path_mtg = Path(path_root / f'outputs\{sky}\combi_{id_combi}')
+    # with open(str(Path(path_root / f'outputs\{sky}\combi_{id_combi}') / f'mtg{str2str(cfg.dates[date_index][1])}.pckl'),
+    #           mode='rb') as f2:
+    #     g, _ = load(f2)
+    #
+    # leaf_ids = get_leaves(g)
+    #
+    # res = {'leaf_id': leaf_ids}
+    #
+    # for s in ('TopPosition', 'BotPosition', 'Length', 'Na', 'ff_sky', 'ff_leaves', 'ff_soil', 'Flux', 'Vcm25',
+    #           'Jm25', 'TPU25', 'Rd', 'u', 'Eabs', 'psi_head', 'gbH', 'Tlc', 'An', 'gs', 'gb', 'E', 'leaf_area'):
+    #     res.update({s: [g.node(v).properties()[s] for v in leaf_ids]})
+    #
+    # DataFrame(res).to_csv(path_mtg / 'summary_props.csv', sep=';', decimal='.', index=False)
+    #
+    # fig, axs = plt.subplots(ncols=2)
+    # display.prop_fun_prop(g, 'gs', 'Eabs', ax=axs[0])
+    # display.prop_fun_prop(g, 'Tlc', 'Eabs', ax=axs[1])
+    # fig.show()
+    # fig.tight_layout()
+    #
+    # x = 1

c06e043f2bfe672d0dc1119d072cf59f4209e9da
rami albasha <rami.albasha@inrae.fr>
2023-05-24 10:53:30 +0200

    add sa param fields

---
+++ sa/config.py
@@ -0,0 +1,56 @@
+from collections import namedtuple
+from itertools import product
+from pathlib import Path
+
+from numpy import linspace
+
+from funcs import calc_reference_mtg_internode_length
+
+
+class ConfigSensitivityAnalysis:
+    def __init__(self, is_on_remote: bool):
+        path_root = Path(__file__).parent.resolve()
+        path_relative = '../../../../mnt/data/hydroshoot/project_mathilde_fspm/sa' if is_on_remote else ''
+
+        self.path_preprocessed_inputs = path_root / ''.join((path_relative, 'path_preprocessed_inputs'))
+        self.path_preprocessed_inputs.mkdir(parents=True, exist_ok=True)
+
+
+class Params:
+    def __init__(self):
+        self.path_reference_digit = Path(__file__).parent.resolve() / 'digit.csv'
+        self._param_values = namedtuple("ParamValues", ['min', 'max', 'bins'])
+        self._reference_internode_length = calc_reference_mtg_internode_length(path_digit=self.path_reference_digit)
+
+        self.internode_length = self.get_param_values(min_value=3, max_value=13.5, nb_values=10)
+        self.internode_scale = self.get_param_values(
+            min_value=self.internode_length.min / self._reference_internode_length,
+            max_value=self.internode_length.max / self._reference_internode_length,
+            nb_values=10)
+        self.limb_inclination = self.get_param_values(min_value=0, max_value=90, nb_values=10)
+        self.leaf_area = self.get_param_values(min_value=50, max_value=350, nb_values=10)
+        self.midrib_length = self._param_values(
+            min=self.calc_midrib_length(leaf_area=self.leaf_area.min),
+            max=self.calc_midrib_length(leaf_area=self.leaf_area.max),
+            bins=[self.calc_midrib_length(leaf_area=v) for v in self.leaf_area.bins])
+
+        self._combinations = list(product(self.internode_scale.bins,
+                                          self.limb_inclination.bins,
+                                          self.midrib_length.bins))
+        self.combinations = list(zip(range(len(self._combinations)), *zip(*self._combinations)))
+
+    def get_param_values(self, min_value: float, max_value: float, nb_values: int):
+        return self._param_values(min=min_value, max=max_value, bins=linspace(min_value, max_value, nb_values))
+
+    @staticmethod
+    def calc_midrib_length(leaf_area: float) -> float:
+        """Calculates the midrib length based on limb area.
+
+        Args:
+            leaf_area: (cm2) area of the limb
+
+        Returns:
+            (cm): midrib length
+
+        """
+        return (leaf_area / 0.0107) ** 0.25
---
+++ sa/funcs.py
@@ -0,0 +1,94 @@
+from pathlib import Path
+
+from hydroshoot import architecture, display
+from numpy import array
+from openalea.mtg import traversal, mtg
+from openalea.plantgl.all import Scene
+
+
+def build_reference_mtg(path_digit: Path) -> tuple[mtg.MTG, Scene]:
+    mtg_vine = architecture.vine_mtg(path_digit)
+
+    for v in traversal.iter_mtg2(mtg_vine, mtg_vine.root):
+        architecture.vine_phyto_modular(mtg_vine, v)
+        architecture.vine_axeII(mtg_vine, v, ci_nfii=0)
+        architecture.vine_petiole(mtg_vine, v)
+        architecture.vine_leaf(mtg_vine, v)
+        architecture.vine_mtg_properties(mtg_vine, v)
+        architecture.vine_mtg_geometry(mtg_vine, v)
+        architecture.vine_transform(mtg_vine, v)
+
+    scene_pgl = display.visu(mtg_vine, def_elmnt_color_dict=True, scene=Scene(), view_result=False)
+
+    return mtg_vine, scene_pgl
+
+
+def get_shoot_node_ids(g: mtg.MTG) -> list:
+    return [v for v in g.VtxList(Scale=2) if g.node(v).label.startswith('sh')]
+
+
+def get_annual_shoot_internode_lengths(g: mtg.MTG) -> list:
+    return [g.node(v2._vid).Length for v1 in get_shoot_node_ids(g) for v2 in g.node(v1).components() if
+            g.node(v2._vid).label.startswith('in')]
+
+
+def calc_reference_mtg_internode_length(path_digit: Path) -> float:
+    reference_mtg, _ = build_reference_mtg(path_digit=path_digit)
+    annual_shoot_internode_lengths = get_annual_shoot_internode_lengths(g=reference_mtg)
+    return sum(annual_shoot_internode_lengths) / len(annual_shoot_internode_lengths)
+
+
+def build_mtg2(path_digit: Path, leaf_inc: float, lim_max: float, scale: float = 1) -> tuple:
+    mtg_vine = architecture.vine_mtg(path_digit)
+
+    for v in traversal.iter_mtg2(mtg_vine, mtg_vine.root):
+        # architecture.vine_phyto_modular(mtg_vine, v)
+        architecture.vine_axeII(mtg_vine, v, ci_nfii=0)
+        architecture.vine_petiole(mtg_vine, v)
+        architecture.vine_leaf(mtg_vine, v, leaf_inc=leaf_inc, lim_max=lim_max)
+
+    mtg_vine = scale_mtg(g=mtg_vine, scale=scale)
+
+    for v in traversal.iter_mtg2(mtg_vine, mtg_vine.root):
+        architecture.vine_mtg_properties(mtg_vine, v)
+        architecture.vine_mtg_geometry(mtg_vine, v)
+        architecture.vine_transform(mtg_vine, v)
+
+    scene_pgl = display.visu(mtg_vine, def_elmnt_color_dict=True, scene=Scene(), view_result=True)
+
+    return mtg_vine, scene_pgl
+
+
+def calc_vectors(g: mtg.MTG, vid_start: int) -> dict:
+    res = {}
+    for vid in list(traversal.iter_mtg2(mtg=g, vtx_id=vid_start))[1:]:
+        n = g.node(vid)
+        p = n.parent()
+        res.update({vid: array(n.TopPosition) - array(p.TopPosition)})
+
+    return res
+
+
+def scale_mtg(g: mtg.MTG, scale: float) -> mtg.MTG:
+    """Scales mtg's TopPosition's.
+
+    Args:
+        g: mtg object
+        scale: scale to be applied (greater than 0)
+
+    Returns:
+        scaled mtg
+
+    Notes:
+        This function MUST be applied BEFORE that of 'vine_mtg_properties()'
+
+    """
+    vectors = {vid_start: calc_vectors(g=g, vid_start=vid_start) for vid_start in get_shoot_node_ids(g=g)}
+    for vid_start in get_shoot_node_ids(g=g):
+        for vid in list(traversal.iter_mtg2(mtg=g, vtx_id=vid_start))[1:]:
+            n = g.node(vid)
+            p = n.parent()
+            n.TopPosition = list(p.TopPosition + vectors[vid_start][vid] * (
+                scale if not n.label.startswith(('L', 'Pet')) else 1))
+
+    return g

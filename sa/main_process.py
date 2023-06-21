62d89d6663a7adade6baee41115d34bb7c816fbf
rami albasha <rami.albasha@inrae.fr>
2023-06-21 15:50:54 +0200

    qdd option to consider the hydrqulic netzork

--- sa/main_process.py
+++ sa/main_process.py
@@ -11,13 +11,17 @@ import config
 
 
 def run_hydroshoot(path_weather: Path, user_params: dict, path_preprocessed_dir: Path, params_architecture: tuple,
-                   path_output_dir: Path, date_info: tuple):
+                   path_output_dir: Path, date_info: tuple, is_hydraulic: bool):
     id_sim, *_ = params_architecture
 
     path_preprocessed = path_preprocessed_dir / f'combi_{id_sim}'
 
     user_params['simulation'].update({'sdate': f"{date_info[1]} 00:00:00"})
     user_params['simulation'].update({'edate': f"{date_info[1]} 23:00:00"})
+
+    if is_hydraulic:
+        user_params["simulation"].update({"hydraulic_structure": True, "negligible_shoot_resistance": False})
+        user_params["exchange"]["par_gs"]['model'] = "misson"
 
     g, scene = architecture.load_mtg(
         path_mtg=str(path_preprocessed / 'initial_mtg.pckl'),
@@ -39,7 +43,7 @@ def run_hydroshoot(path_weather: Path, user_params: dict, path_preprocessed_dir:
         scene=scene,
         write_result=True,
         path_output=path_output / 'time_series.csv',
-        psi_soil=-0.01,
+        psi_soil_init=-0.01,
         form_factors=static_inputs['form_factors'],
         leaf_nitrogen=static_inputs['Na'],
         leaf_ppfd=dynamic_inputs)
@@ -57,14 +61,19 @@ def mp(sim_args: Iterable, nb_cpu: int = 2):
 
 
 if __name__ == '__main__':
+    is_consider_hydraulic_network: bool = True
+
     path_root = Path(__file__).parent
     cfg = config.ConfigSensitivityAnalysis(is_on_remote=False)
     params = config.Params()
     params_archi = params.combinations
 
+    path_outputs = cfg.path_outputs.parent / "_".join((cfg.path_outputs.name,
+                                                       f"{'psi' if is_consider_hydraulic_network else 'vpd'}"))
+
     time_on = datetime.now()
-    mp(sim_args=product(
-        [cfg.path_weather], [cfg.params], [cfg.path_preprocessed_inputs], params_archi, [cfg.path_outputs], cfg.dates),
-        nb_cpu=12)
+    mp(sim_args=product([cfg.path_weather], [cfg.params], [cfg.path_preprocessed_inputs], params_archi,
+                        [cfg.path_outputs], cfg.dates, [is_consider_hydraulic_network]),
+       nb_cpu=12)
     time_off = datetime.now()
     print(f"--- Total runtime: {(time_off - time_on).seconds} sec ---")
